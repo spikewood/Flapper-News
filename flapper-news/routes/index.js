1fc5208472ea3d46e09ff880344247c487003685
Mike Wood <wood.michaelthomas@gmail.com>
2017-04-18 22:27:22 -0700

    Added routes for post and get
    post and get routes for the posts have been made

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -3,7 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
+});
+
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next){
+	Post.find(function(err, posts){
+		if (err) { next(err); }
+
+		res.json(posts);
+	});
+});
+
+router.post('/posts', function(req, res, next){
+	var post = new Post(req.body);
+
+	post.save(function(err, post){
+		if (err) { return next(err); }
+
+		res.json(post);
+	});
 });
 
 module.exports = router;
