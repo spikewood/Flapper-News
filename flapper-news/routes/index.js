f36a31f76fe2cac1f1474495c0fbd8e4198b7184
Mike Wood <wood.michaelthomas@gmail.com>
2017-04-18 22:51:24 -0700

    Can upvote
    Added up vote functionality

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -28,4 +28,28 @@ router.post('/posts', function(req, res, next){
 	});
 });
 
+router.param('post', function(req, res, next, id){
+	var query = Post.findById(id);
+
+	query.exec(function(err, post){
+		if (err) { return next(err); }
+		if (!post) { return new Erron("can't find post"); }
+
+		req.post = post;
+		return next();
+	})
+});
+
+router.get('/posts/:post', function(req, res) {
+	res.json(req.post);
+});
+
+router.put('/posts/:post/upvote', function(req, res, next) {
+	req.post.upvote(function(err, post) {
+		if (err) { return next(err); }
+
+		res.json(post);
+	});
+});
+
 module.exports = router;
