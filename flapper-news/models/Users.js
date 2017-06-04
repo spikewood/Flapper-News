96899cb06042d4abbc58fde1ab6fa880f872f367
Mike Wood <wood.michaelthomas@gmail.com>
2017-06-04 08:13:57 -0700

    Added auth
    added auth

---
+++ flapper-news/models/Users.js
@@ -0,0 +1,48 @@
+//Always need mongoose
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+// Schema for the Mongo DB goes here
+
+// Each user will have a username, a password (stored in the hash), and a salt to encrypt the password
+var UserSchema = new mongoose.Schema({
+	username: {type:String, lowercase:true, unique:true},
+	hash: String,
+	salt: String
+});
+
+// Functions to run in the schema go here
+
+// setPassword method will set the password by salting the password and encrypting it
+UserSchema.methods.setPassword = function(password) {
+	// salt with 16 bytes of random
+	this.salt = crypto.randomBytes(16).toString('hex');
+
+	// encrypt password with the salt and hash 1000 times
+	this.hash = crypto.pbkdf2Sync(password, salt, 1000, 64).toString('hex');
+}
+
+// validPassword method will set the password by salting the password and encrypting it
+UserSchema.methods.validPassword = function(password) {
+	// encrypt password with the salt and hash 1000 times
+	var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+	return this.hash === hash;
+}
+
+// generateJWT method genrates a web token good for 60 days
+UserSchema.methods.generateJWT = function() {
+	var today = new Date();
+	var exp = new Date(today);
+	exp.setDate(today.getDate() + 60);
+
+	return jwt.sign({
+		_id: this._id,
+		username: this.username,
+		exp: parseInt(exp.getTime() / 1000)
+	}, 'SECRET');
+}
+
+//Model compilation
+mongoose.model('User', UserSchema);
--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -11,23 +11,72 @@ function($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
     	url: '/home',
     	templateUrl: '/home.html',
-    	controller: 'MainCtrl'
+    	controller: 'MainCtrl',
+    	resolve: {
+    		postPromise: ['posts', function(posts){
+    			return posts.getAll();
+    		}]
+    	}
     });
 
     $stateProvider.state('posts', {
-    	url: '/posts/{id}',
-    	templateUrl: '/posts.html',
-    	controller: 'PostsCtrl'
-    });
+		  url: '/posts/{id}',
+		  templateUrl: '/posts.html',
+		  controller: 'PostsCtrl',
+		  resolve: {
+		    post: ['$stateParams', 'posts', function($stateParams, posts) {
+		      return posts.get($stateParams.id);
+		    }]
+		  }
+		});
 
 	$urlRouterProvider.otherwise('home');
 }]);
 
 // app factory for storing data between views
-app.factory('posts', [function(){
+app.factory('posts', ['$http', function($http){
   var o = {
     posts: []
   };
+
+  o.getAll = function() {
+  	return $http.get('/posts').success(function(data) {
+  		angular.copy(data, o.posts);
+  	});
+  };
+
+  o.get = function(id) {
+	  return $http.get('/posts/' + id).then(function(res){
+	    return res.data;
+	  });
+	};
+
+	o.addComment = function(id, comment) {
+		return $http.post('/posts/' + id + '/comments', comment);
+	};
+
+  o.create = function(post) {
+  	return $http.post('/posts', post).success(function(data){
+    	o.posts.push(data);
+  	});
+	};
+
+  o.upvote = function(post) {
+  return $http.put('/posts/' + post._id + '/upvote')
+    .success(function(data){
+      post.upvotes += 1;
+    });
+	};
+
+	o.upvoteComment = function(post, comment) {
+	  console.log('this is the: ',post);
+	  console.log('this is the: ',comment);
+	  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
+	    .success(function(data){
+	      comment.upvotes += 1;
+	    });
+	};
+  
   return o;
 }]);
 
@@ -46,39 +95,48 @@ app.controller('MainCtrl', [
 		$scope.addPost = function(){
 			// prevent clicks of button from submitting empty posts
 			if ($scope.title === '') { return;}
-			// fill ouy the post
-			$scope.posts.push({
-  				title: $scope.title,
-  				link: $scope.link,
-  				upvotes: 0,
-  				comments: []
-			});
+			// fill out the post
+			posts.create({
+				title: $scope.title,
+				link: $scope.link,
+  		});
 	  		$scope.title = '';
 	  		$scope.link = '';
-		}
+		};
 
-		// increment the upvotes on click
-		$scope.incrementUpvotes = function(post){
-	  		post.upvotes ++;
-		}
+		$scope.incrementUpvotes = function(post) {
+  		posts.upvote(post);
+		};
+
 }]);
 
 // app controller for the posts
 app.controller('PostsCtrl', [
 	'$scope',
-	'$stateParams',
 	'posts',
-	function($scope, $stateParams, posts){
-		$scope.post = posts.posts[$stateParams.id];
-	
+	'post',
+	function($scope, posts, post){
+		$scope.post = post;
+
 		$scope.addComment = function(){
-	  		if($scope.body === '') { return; }
-	  		$scope.post.comments.push({
-	    		body: $scope.body,
-	   			author: 'user',
-	    		upvotes: 0
-	  		});
-	  		$scope.body = '';
-		}
+		  if($scope.body === '') { return; }
+		  posts.addComment(post._id, {
+		    body: $scope.body,
+		    author: 'user',
+		  }).success(function(comment) {
+		    $scope.post.comments.push(comment);
+		  });
+		  $scope.body = '';
+		};
+
+		$scope.incrementUpvotes = function(comment){
+  		console.log('before the upvote call post is: ', post);
+  		console.log('before the upvote call posts.upvoteComment is: ', posts.upvoteComment);
+  		console.log('before the upvote call comment is: ', comment);
+  		posts.upvoteComment(post, comment);
+  		console.log('after the upvote call post is: ', post);
+  		console.log('after the upvote call posts is: ', posts);
+  		console.log('after the upvote call comment is: ', comment);
+		};
 	}
 ]);
