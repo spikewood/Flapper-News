dc24b65734034d50afc138ed2ce732df6567650a
Mike Wood <wood.michaelthomas@gmail.com>
2017-06-04 14:26:24 -0700

    Adding registration and login pages

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -1,40 +1,156 @@
 var app = angular.module('flapperNews', ['ui.router']);
 
 // application configuration values
-app.config([
-'$stateProvider',
-'$urlRouterProvider',
+app
+.config([
+	'$stateProvider',
+	'$urlRouterProvider',
 
-//State controller for the application states
-function($stateProvider, $urlRouterProvider) {
-	
-    $stateProvider.state('home', {
-    	url: '/home',
-    	templateUrl: '/home.html',
-    	controller: 'MainCtrl',
-    	resolve: {
-    		postPromise: ['posts', function(posts){
-    			return posts.getAll();
-    		}]
-    	}
-    });
+	//State controller for the application states
+	function($stateProvider, $urlRouterProvider) {	
+		$stateProvider
 
-    $stateProvider.state('posts', {
-		  url: '/posts/{id}',
-		  templateUrl: '/posts.html',
-		  controller: 'PostsCtrl',
-		  resolve: {
-		    post: ['$stateParams', 'posts', function($stateParams, posts) {
-		      return posts.get($stateParams.id);
-		    }]
-		  }
+			.state('home', {
+	    	url: '/home',
+	    	templateUrl: '/home.html',
+	    	controller: 'MainCtrl',
+	    	resolve: {
+	    		postPromise: ['posts', function(posts){
+	    			return posts.getAll();
+	    		}]
+	    	}
+	    })
+
+	    .state('posts', {
+			  url: '/posts/{id}',
+			  templateUrl: '/posts.html',
+			  controller: 'PostsCtrl',
+			  resolve: {
+			    post: ['$stateParams', 'posts', function($stateParams, posts) {
+			      return posts.get($stateParams.id);
+			    }]
+			  }
+			})
+
+			.state('login', {
+			  url: '/login',
+			  templateUrl: '/login.html',
+			  controller: 'AuthCtrl',
+			  onEnter: ['$state', 'auth', function($state, auth){
+			    if(auth.isLoggedIn()){
+			      $state.go('home');
+			    }
+			  }]
+			})
+
+			.state('register', {
+			  url: '/register',
+			  templateUrl: '/register.html',
+			  controller: 'AuthCtrl',
+			  onEnter: ['$state', 'auth', function($state, auth){
+			    if(auth.isLoggedIn()){
+			      $state.go('home');
+			    }
+			  }]
+			})
+
+		$urlRouterProvider.otherwise('home');
+	}])
+
+.factory('auth', ['$http', '$window', function($http, $window){
+	var auth = {};
+
+	// saveToken stores the generated token into local storage to keep folks logged in
+	auth.saveToken = function (token){
+	  $window.localStorage['flapper-news-token'] = token;
+	};
+
+	auth.getToken = function (){
+	  return $window.localStorage['flapper-news-token'];
+	};
+
+	//isLoggedIn compares the expiration date from the window vs today's date
+	auth.isLoggedIn = function(){
+	  var token = auth.getToken();
+
+	  if(token){
+	    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+	    return payload.exp > Date.now() / 1000;
+	  } else {
+	    return false;
+	  }
+	};
+
+	// username pulls the username from the window if the user is logged in
+	auth.currentUser = function(){
+	  if(auth.isLoggedIn()){
+	    var token = auth.getToken();
+	    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+	    return payload.username;
+	  }
+	};
+
+	// register saves the token to the window
+	auth.register = function(user){
+	  return $http.post('/register', user).success(function(data){
+	    auth.saveToken(data.token);
+	  });
+	};
+
+	// login saves the token to the window
+	auth.login = function(user) {
+		return $http.post('/login', user).success(function(data) {
+			auth.saveToken(data, token);
 		});
+	};
 
-	$urlRouterProvider.otherwise('home');
-}]);
+	// logout removes the token from the window
+	auth.logout = function() {
+		$window.localStorage.removeItem('flapper-news-token');
+	};
+
+	return auth;
+}])
+
+// Controller for Authentication
+.controller('AuthCtrl', [
+	'$scope',
+	'$state',
+	'auth',
+	function($scope, $state, auth){
+	  $scope.user = {};
+
+	  $scope.register = function(){
+	    auth.register($scope.user).error(function(error){
+	      $scope.error = error;
+	    }).then(function(){
+	      $state.go('home');
+	    });
+	  };
+
+	  $scope.logIn = function(){
+	    auth.logIn($scope.user).error(function(error){
+	      $scope.error = error;
+	    }).then(function(){
+	      $state.go('home');
+	    });
+	  };
+}])
+
+// Auth nav controller
+.controller('NavCtrl', [
+	'$scope',
+	'auth',
+	function($scope, auth){
+	  $scope.isLoggedIn = auth.isLoggedIn;
+	  $scope.currentUser = auth.currentUser;
+	  $scope.logOut = auth.logOut;
+}])
 
 // app factory for storing data between views
-app.factory('posts', ['$http', function($http){
+.factory('posts', ['$http', function($http){
   var o = {
     posts: []
   };
@@ -78,10 +194,10 @@ app.factory('posts', ['$http', function($http){
 	};
   
   return o;
-}]);
+}])
 
 // app controller for the Main View
-app.controller('MainCtrl', [
+.controller('MainCtrl', [
 	'$scope',
 	'posts',
 
@@ -108,10 +224,10 @@ app.controller('MainCtrl', [
   		posts.upvote(post);
 		};
 
-}]);
+}])
 
 // app controller for the posts
-app.controller('PostsCtrl', [
+.controller('PostsCtrl', [
 	'$scope',
 	'posts',
 	'post',
@@ -139,4 +255,4 @@ app.controller('PostsCtrl', [
   		console.log('after the upvote call comment is: ', comment);
 		};
 	}
-]);
+])
